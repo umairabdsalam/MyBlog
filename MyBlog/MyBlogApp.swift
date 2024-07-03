95980b952b8e344d873314d1542ac22d8390cf4f
Umair Abdus Salam <umairabdsalam@gmail.com>
2024-07-03 15:17:29 +0100

    Initial Commit

---
+++ MyBlog/MyBlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBlogApp.swift
+//  MyBlog
+//
+//  Created by Umair Salam on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MyBlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
